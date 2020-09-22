8c6d652c7f217cb8064bb9cda2cf63415bf08884
geunwoo <960722rjsdn@naver.com>
2020-09-22 17:33:19 +0900

    dummytest

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Ahngeunwoo on 2020/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //click event
+
+    @IBAction func Click_moveBtn(_ sender: Any) {
+        print("Click MOVE")
+    }
+    
+}
+
