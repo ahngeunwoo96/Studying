53a72deb5719d932b89822fcc039704abb91e1e5
geunwoo <960722rjsdn@naver.com>
2020-10-14 23:43:34 +0900

    SwiftUI_stack_practice_tutorial

---
+++ SwiftUI_stack_practice_tutorial/SwiftUI_stack_practice_tutorial/MyList.swift
@@ -0,0 +1,70 @@
+//
+//  MyList.swift
+//  SwiftUI_stack_practice_tutorial
+//
+//  Created by Ahngeunwoo on 2020/10/09.
+//
+
+import SwiftUI
+
+struct MyList : View {
+    
+    init() {
+        if #available(iOS 14.0, *){
+            
+        }else {
+            UITableView.appearance().tableFooterView = UIView()
+        }
+        
+        UITableView.appearance().separatorStyle = .none
+    }
+
+    var body: some View {
+        //방법 1
+//        List{
+//            Text("My List")
+//            Text("My List")
+//            Text("My List")
+//            Text("My List")
+//            Text("My List")
+//        }
+        //방법 2
+//        List {
+//            ForEach(1...10, id: \.self) {
+//                Text("My List\($0)")
+//            }
+//        }
+        List {
+            
+            Section(header: Text("오늘의 첫번째 할 일.")
+                    ,footer: Text("footer")
+            
+            ) {
+                ForEach(1...3, id: \.self) { itemIndex in
+                    //Text("My List\(itemIndex)")
+                    MyCard(icon: "book.fill", title: "책 읽기\(itemIndex)", start: "1 PM", end: "3 PM", bgColor: Color.green)
+                }
+            }
+            .listRowInsets(EdgeInsets.init(.init(top: 10, leading: 10, bottom: 10, trailing: 10)))
+            
+            Section(header: Text("오늘의 두번째 할 일.")) {
+                ForEach(1...3, id: \.self) { itemIndex in
+                    //Text("My List\(itemIndex)")
+                    MyCard(icon: "gamecontroller.fill", title: "롤 한판하기\(itemIndex)", start: "3 PM", end: "4 PM", bgColor: Color.orange)
+                }
+            }
+            .listRowInsets(EdgeInsets.init(.init(top: 10, leading: 10, bottom: 10, trailing: 10)))
+            .listRowBackground(Color.yellow)
+        }
+        .listStyle(GroupedListStyle())
+        //.listStyle(PlainListStyle()) // list의 기본 스타일
+
+        
+    }
+}
+
+struct MyList_Previews: PreviewProvider {
+    static var previews: some View {
+        MyList()
+    }
+}
