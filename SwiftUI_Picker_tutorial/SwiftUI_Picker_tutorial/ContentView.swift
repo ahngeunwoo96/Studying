426b11f6225152a74cf798475857506ce5e7c8b6
geunwoo <960722rjsdn@naver.com>
2020-10-28 01:06:54 +0900

    SwiftUI_PickerView_tutorial

--- SwiftUI_Picker_tutorial/SwiftUI_Picker_tutorial/ContentView.swift
+++ SwiftUI_Picker_tutorial/SwiftUI_Picker_tutorial/ContentView.swift
@@ -7,24 +7,36 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     
     @State private var selectionValue = 0
     
-    @State private var pickerColor : String = "그린"
+    let myColorArray = ["레드", "그린", "블루"]
+    
+    func changeColor(index: Int) -> Color {
+        switch index {
+        case 0:
+            return Color.red
+        case 1:
+            return Color.green
+        case 2:
+            return Color.blue
+        default:
+            return Color.red
+        }
+    }
     
     var body: some View {
-        VStack(alignment : .center, spacing : 0) {
+        VStack(alignment : .center) {
+            
             Circle()
                 .frame(width :50, height : 50)
-                .foregroundColor(Color.\(pickerColor))
-                .padding(.bottom, 10)
+                .foregroundColor(changeColor(index: selectionValue))
             
             Text("세그먼트 value : \(selectionValue)")
-                .padding(.bottom, 10)
             
-            Text("선택된 색깔 : \(pickerColor)")
-                .padding(.bottom, 10)
+            Text("선택된 색깔 : \(myColorArray[selectionValue])")
             
             Picker("피커", selection: $selectionValue, content: {
                 Text("레드").tag(0)
@@ -33,12 +45,15 @@ struct ContentView: View {
             })
             .pickerStyle(SegmentedPickerStyle())
             
-            Picker("피커", selection: $pickerColor, content: {
+            Picker("피커", selection: $selectionValue, content: {
                 Text("레드").tag(0)
                 Text("그린").tag(1)
                 Text("블루").tag(2)
             })
             .frame(width : 50, height : 100)
+            .clipped()
+            .padding(10)
+            .border(changeColor(index: selectionValue), width : 10)
             
             
         }
